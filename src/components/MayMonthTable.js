1dd86be86d1ba972829c55f4d12895233339fa07
Ji Park <jiyongprk@gmail.com>
2022-09-20 20:37:33 -0400

    th test done

--- src/components/MayMonthTable.js
+++ src/components/MayMonthTable.js
@@ -3,16 +3,16 @@ import React from 'react'
 export default function MayMonthTable({ sortedMayTable, calculateMayTotalRewards }) {
     return (
         <div>
-            <h1>MAY</h1>
+            <h1 data-testid='header-may'>MAY</h1>
             {/* customer 123 */}
             <table border="1"  >
                 <thead >
                     <tr>
-                        <th>Customer ID</th>
-                        <th>Transaction ID</th>
-                        <th>Date</th>
-                        <th>Price</th>
-                        <th>Rewards</th>
+                        <th data-testid='th-customerid'>Customer ID</th>
+                        <th data-testid='th-transactionid'>Transaction ID</th>
+                        <th data-testid='th-date'>Date</th>
+                        <th data-testid='th-price'>Price</th>
+                        <th data-testid='th-rewards'>Rewards</th>
                     </tr>
                 </thead>
                 <tbody>
--- src/components/MayMonthTable.test.js
+++ src/components/MayMonthTable.test.js
@@ -35,10 +35,35 @@ describe(`MayMonthTable should render customer's may information in tables`, () 
         const { asFragment } = render(<MayMonthTable
             sortedMayTable={sortedMayTable}
             calculateMayTotalRewards={calculateMayTotalRewards}
-
         />)
         expect(asFragment()).toMatchSnapshot()
     })
 
+    it('header should be MAY', () => {
+        render(<MayMonthTable
+            sortedMayTable={sortedMayTable}
+            calculateMayTotalRewards={calculateMayTotalRewards}
+        />)
+        const headerMay = screen.getByTestId('header-may').textContent
+        expect(headerMay).toBe('MAY')
+    })
+
+    it('table header should be customerid,transactionid,date,price,rewards', () => {
+        render(<MayMonthTable
+            sortedMayTable={sortedMayTable}
+            calculateMayTotalRewards={calculateMayTotalRewards}
+        />)
+        const thCustomerID = screen.getByTestId('th-customerid').textContent
+        const thTransactionID = screen.getByTestId('th-transactionid').textContent
+        const thDate = screen.getByTestId('th-date').textContent
+        const thPrice = screen.getByTestId('th-price').textContent
+        const thRewards = screen.getByTestId('th-rewards').textContent
+
+        expect(thCustomerID).toBe('Customer ID')
+        expect(thTransactionID).toBe('Transaction ID')
+        expect(thDate).toBe('Date')
+        expect(thPrice).toBe('Price')
+        expect(thRewards).toBe('Rewards')
+    })
 
 })
